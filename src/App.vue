949682ba07cf385d9979ef0e8fced3dd8df9ae7c
developer abir <developerabir40@gmail.com>
2022-02-23 19:32:24 +0600

    vue Cli

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header :fastName="fName" @updateValue="fName = $event" />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/Header_footer/Header.vue";
+import Footer from "./components/Header_footer/Footer.vue";
 export default {
-  name: 'App',
+  data() {
+    return {
+      fName: "jhon",
+      lName: "doe",
+      age: 56,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Footer,
+  },
+};
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.myAge {
+  color: red;
+}
+.hello:hover {
+  font-weight: 900;
+  color: palevioletred;
+  animation: all;
+}
+body {
+  font-family: "Roboto", sans-serif;
 }
 </style>
---
+++ src/components/Header_footer/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>fastName: {{ fastName }}</p>
+    <p>lasttName: {{ lastName }}</p>
+    <p>Client Age : {{ age }}</p>
+    <button @click="updateName">Update Name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    fastName: String,
+    lastName: String,
+    age: Number,
+  },
+  methods: {
+    updateName() {
+      this.$emit("updateValue", "Kalifaae e");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
